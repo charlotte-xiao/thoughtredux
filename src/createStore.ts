2e8d1023259a5bc249faf45dd16669e10ca4770e
xiao sa <sa.xiao@thoughtworks.com>
2022-11-13 18:55:09 +0800

    <feat>: CreateStore Function with dispatch and getState

---
+++ src/createStore.ts
@@ -0,0 +1,33 @@
+function createStore(reducer, preloadState, enhancer) {
+
+  let currentReducer = reducer;
+  let currentState = preloadState;
+  let isDispatching = false;
+
+  function getState() {
+    if (isDispatching) {
+      throw new Error("in dispatching, can't get state");
+    }
+    return currentState;
+  }
+
+  function dispatch(action) {
+    if (isDispatching) {
+      throw new Error("in dispatching, can't dispatch");
+    }
+
+    try {
+      isDispatching = true;
+      currentState = currentReducer(currentState, action);
+    } finally {
+      isDispatching = false;
+    }
+    return action;
+  }
+
+  return {
+    dispatch,
+    getState,
+  }
+
+}
